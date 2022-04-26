c4886219d4f6cf323c05c55a32559d813b94acdc
Owen Moore <owenmoor097@ed.amdsb.ca>
2022-04-26 09:59:36 -0400

    1

--- XMLWeather/ForecastScreen.cs
+++ XMLWeather/ForecastScreen.cs
@@ -20,12 +20,12 @@ namespace XMLWeather
         public void displayForecast()
         {
             date1.Text = Form1.days[1].date;
-            min1.Text = Form1.days[1].tempLow;
-            max1.Text = Form1.days[1].tempHigh;
+            min1.Text = $"{Form1.days[1].tempLow} degrees";
+            max1.Text = $"{Form1.days[1]}.tempHigh";
 
             date2.Text = Form1.days[2].date;
-            min2.Text = Form1.days[2].tempLow;
-            max2.Text = Form1.days[2].tempHigh;
+            min2.Text = $"{Form1.days[2].tempLow} degrees";
+            max2.Text = $"{Form1.days[2].tempHigh} degrees";
 
 
 
